de79c0004dec2738f6839bceafe76b3a5fcaf6b4
xantos008 <amote1234@mail.ru>
2021-04-01 17:43:22 +0700

    Premium event

--- main.py
+++ main.py
@@ -14,3 +14,12 @@ r = requests.post(url, data=json.dumps(body), headers=headers)
 data = r.json()
 
 token = data['token_type'] + " " + data['access_token']
+
+url = 'http://salty-reef-38656.herokuapp.com//events/premium_event'
+body = {
+    'email': 'your@user.email'
+}
+# headers for json response
+headers = {'Content-type': 'application/json', 'authorization': token}
+
+r = requests.post(url, data=json.dumps(body), headers=headers)
