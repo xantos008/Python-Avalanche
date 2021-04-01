1c163a80eb18689af9939973b8c6c75306b900f3
xantos008 <amote1234@mail.ru>
2021-04-01 17:34:43 +0700

    Create main.py

---
+++ main.py
@@ -0,0 +1,16 @@
+import requests
+import json
+
+url = 'https://useavalanche.us.auth0.com/oauth/token'
+body = {
+    'client_id': 'ertuRwxTMB6k8cV5LIZVg5nWTosIyVUH',
+    'client_secret': 'Ls2aqX4pEp8npwx3yk6OeZq8ETbtdWPYqOcnepaKIHIr1u0U5b4XoDXsJaKyufeP',
+    'audience': 'https://useavalanche.us.auth0.com/api/v2/',
+    'grant_type': 'client_credentials'}
+# headers for json response
+headers = {'content-type': 'application/json'}
+
+r = requests.post(url, data=json.dumps(body), headers=headers)
+data = r.json()
+
+token = data['token_type'] + " " + data['access_token']
